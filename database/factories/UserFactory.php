50c99d9ad0be6e64de230f0044b47e59014aa0ca
Colston <bodoycolstond@gmail.com>
2022-04-10 03:57:19 +0800

    Added tweets and users table.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,16 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            "name" => $this->faker->name(),
+            "username" => $this->faker->unique()->userName(),
+            "email" => $this->faker->unique()->safeEmail(),
+            "email_verified_at" => now(),
+            "password" =>
+                '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            "avatar" =>
+                "https://i.pravatar.cc/150?img=" .
+                $this->faker->numberBetween(1, 70),
+            "remember_token" => Str::random(10),
         ];
     }
 
@@ -32,7 +37,7 @@ class UserFactory extends Factory
     {
         return $this->state(function (array $attributes) {
             return [
-                'email_verified_at' => null,
+                "email_verified_at" => null,
             ];
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
+});
+
+Route::get("/tweets", function () {
+    return Tweet::with("user:id,name,username,avatar")
+        ->latest()
+        ->get();
 });
